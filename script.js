a97b952edc59d632a4fa99219b09ebcc033c456d
Kevin <Atnnetworkk@gmail.com>
2020-05-16 22:10:02 +0200

    Load image, and get the average color

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.height = innerHeight;
-canvas.width = innerWidth;
+canvas.height = innerHeight / 2;
+canvas.width = innerWidth / 2;
 ctx.fillStyle = "black";
 ctx.rect(80, 80, 80, 80);
 ctx.fill();
 
-const image = new Image();
+let image = new Image();
 
-image.src = "./img/test.png";
-image.onload = function () {
-  ctx.drawImage(image, 0, 0);
+function analyseImg(image) {
+  let pixelArr = { r: 0, g: 0, b: 0 };
+  let pixels = ctx.getImageData(0, 0, image.width, image.height);
+  console.log(pixels);
+  let count = 0;
+  for (let pixel = 0; pixel < pixels.data.length; pixel += 20) {
+    pixelArr.r += pixels.data[pixel];
+    pixelArr.g += pixels.data[pixel + 1];
+    pixelArr.b += pixels.data[pixel + 2];
+    count++;
+  }
+  pixelArr.r = Math.floor(pixelArr.r / count);
+  pixelArr.g = Math.floor(pixelArr.g / count);
+  pixelArr.b = Math.floor(pixelArr.b / count);
+  console.log(pixelArr);
+  let hex = fullColorHex(pixelArr.r, pixelArr.g, pixelArr.b);
+  changeBg(hex);
+}
+
+var rgbToHex = function (rgb) {
+  let hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+    hex = "0" + hex;
+  }
+  return hex;
 };
+var fullColorHex = function (r, g, b) {
+  let red = rgbToHex(r);
+  let green = rgbToHex(g);
+  let blue = rgbToHex(b);
+  return red + green + blue;
+};
+
+function displayFile(e) {
+  image.src = e.target.result;
+  image.onload = function () {
+    document.getElementById("input").src = image.src;
+    ctx.drawImage(image, 0, 0);
+    analyseImg(image);
+  };
+}
+
+function LoadNewImage(e) {
+  let uploaded = e.target.files[0];
+  let read = new FileReader();
+  read.addEventListener("load", displayFile);
+  read.readAsDataURL(uploaded);
+}
+
+function changeBg(hex) {
+  console.log(hex);
+  document.body.style.backgroundColor = "#" + hex;
+}
+
+document.getElementById("load").addEventListener("change", function (e) {
+  LoadNewImage(e);
+});
